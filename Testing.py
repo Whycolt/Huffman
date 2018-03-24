fe36c53e5bbe483ca5662f5674dd1fb3bfd8690d
Yuting Liu <yuting9@gmail.com>
2018-03-24 19:35:50 -0400

    Fixed Code

--- Testing.py
+++ Testing.py
@@ -1,5 +1,4 @@
-from nodes import HuffmanNode, ReadNode
-
+from nodes import HuffmanNode
 def get_codes(tree):
     """ Return a dict mapping symbols from Huffman tree to codes.
 
@@ -11,14 +10,17 @@ def get_codes(tree):
     >>> d == {3: "0", 2: "1"}
     True
     """
+    print(tree)
     return parse_tree(tree, "")
         
 def parse_tree(node, code, codict={}):
     dict1, dict2 = {}, {}
-    if node.is_leaf:
+    if node.is_leaf():
+        print("found leaf")
         codict[node.symbol] = code
         return codict
     if node.left:
+        print(node.left)
         dict1 = parse_tree(node.left, code+"0", codict)
     if node.right:
         dict2 = parse_tree(node.right, code+"1", codict)
@@ -26,6 +28,7 @@ def parse_tree(node, code, codict={}):
 
 tree = HuffmanNode(None, HuffmanNode(3), HuffmanNode(2))
 d = get_codes(tree)
+print(d)
 print(d == {3: "0", 2: "1"})
 
 def avg_length(tree, freq_dict):
@@ -56,5 +59,4 @@ freq = {3: 2, 2: 7, 9: 1}
 left = HuffmanNode(None, HuffmanNode(3), HuffmanNode(2))
 right = HuffmanNode(9)
 tree = HuffmanNode(None, left, right)
-print(avg_length(tree, freq))
-1.9
+print(get_codes(tree) == freq)
